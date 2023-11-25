23ce32deb9cb89a68769cbc503aecc0c510c3360
damjali <138444138+damjali@users.noreply.github.com>
2023-11-25 17:25:30 +0800

    Update DuitRIa.java

--- src/duitria/DuitRIa.java
+++ src/duitria/DuitRIa.java
@@ -14,7 +14,7 @@ public class DuitRIa {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Testing1");
+        System.out.println("Testing2");
     }
     
 }
