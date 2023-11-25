f13f0fb250c5e3ba847915825ed4cadc1d266ec3
Adam Razali <Adam Razali@LAPTOP-SG0SLKBU>
2023-11-25 17:24:15 +0800

    First Commit Test

--- src/duitria/DuitRIa.java
+++ src/duitria/DuitRIa.java
@@ -14,7 +14,7 @@ public class DuitRIa {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Testing");
+        System.out.println("Testing1");
     }
     
 }
